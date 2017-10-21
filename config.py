add94e6c4e0ed3902adb0113f9e914271f61f7c9
calvinti12 <calvinti12@gmail.com>
2017-10-21 00:14:28 -0500

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 # Database setup
-SQLALCHEMY_DATABASE_URI = postgres://sicesapiuadffa:e85752c6f072f1261e99c63ac31637b84c41fe62d9fcd3385227faa36f826248@ec2-54-221-196-253.compute-1.amazonaws.com:5432/d12ef7nd0hamvv
+SQLALCHEMY_DATABASE_URI ='YOUR_DATABASE_URI'
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
